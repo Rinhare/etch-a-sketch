d25bef0a537dc816f9cf9db6e37cc79926ebd0eb
Jusu <jusurinhare@gmail.com>
2023-11-20 19:21:43 +0200

    CSS grid testing

--- main.js
+++ main.js
@@ -4,21 +4,31 @@ grid = function(squaresValue) {
     let squaresAmount = 0;
     
     if(squaresValue !== undefined) {
-        squaresAmount = squaresValue;
+        squaresAmount = squaresValue ;
         console.log("value of squares " + squaresAmount);
     }
     else {
-        squaresAmount = 16;
+        squaresAmount = 100;
         console.log("value of squares " + squaresAmount);
     }
     
     const container = document.querySelector("#container");
     
     // loop for creating squares.  
-    for(i = 0; squaresAmount >= i; i++) {
-        const square = document.createElement("div");
-        container.appendChild(square);
-        square.textContent = "test";
+    for(i = 1; squaresAmount >= i; i++) {
+        const columnSquare = document.createElement("div");
+        columnSquare.setAttribute("id", "column-" + i);
+        container.appendChild(columnSquare);
+
+        const rowSquareSelector = document.querySelector("#column-" + i)
+
+        for(j = 1; squaresAmount >=j; j++ ) {
+            const rowSquare = document.createElement("div")
+            rowSquare.setAttribute("id", "row" )
+            rowSquareSelector.appendChild(rowSquare);
+            // rowSquare.textContent = "test" + j; 
+        }
+
     }
     
     
